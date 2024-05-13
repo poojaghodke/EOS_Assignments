bec5d25d5eefb177fb8c1ad597b8eb7af5f78343
poojaghodke <sonarpoojar@gmail.com>
2024-05-13 16:22:30 +0530

    Assignments

---
+++ day08/assignment3.c
@@ -0,0 +1,33 @@
+#include<unistd.h>
+#include<stdio.h>
+#include <sys/wait.h>
+
+int main()
+{
+int ret,s,i,j;
+i=1;
+while(1)
+{
+	ret =fork();
+	if(ret==0)
+	{
+		printf("hiiii....!\n");
+		_exit(0);
+	}
+    else if(ret==-1)
+	{
+		break;
+	}
+	else
+	{
+		i++;
+		printf("child count: %d\n",i);
+	}
+}
+for(j=1;j<=i;j++)
+{
+wait(&s);
+}
+return 0;
+
+}
